1a335ae0d20397cff3f2f5fa881bee36af25f0c9
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-06-13 15:42:17 -0300

    feat: txt_import

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            sys.stderr.write("Formato inválido")
+
+        with open(path_file, 'r') as file:
+            lines = file.read().splitlines()
+            return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
