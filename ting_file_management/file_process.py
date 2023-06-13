bab49652ea8b403c35fc82db2659d3d51bba9cc7
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-06-13 15:52:42 -0300

    feat: process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
+    for index in range(len(instance)):
+        data = instance.search(index)
+        if data["nome_do_arquivo"] == path_file:
+            return
+
+    lines = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
