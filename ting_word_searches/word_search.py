8fcd16dbefb404d95247fb079c4fc3a921f26035
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-06-13 16:07:11 -0300

    feat: exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
+from ting_file_management.queue import Queue
+
+
+def search_word(word: str, instance: Queue, include_content: bool = False):
+    data = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+        if any(word in line.lower() for line in file["linhas_do_arquivo"]):
+            result = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": [
+                    {"linha": i + 1, "conteudo": line}
+                    if include_content
+                    else {"linha": i + 1}
+                    for i, line in enumerate(file["linhas_do_arquivo"])
+                    if word in line.lower()
+                ],
+            }
+            data.append(result)
+    return data
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    return search_word(word, instance, include_content=False)
 
 
 def search_by_word(word, instance):
