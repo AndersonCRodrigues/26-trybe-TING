0cfafd23fdac66bf89f643d32c315a7baaa88d4a
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-06-13 15:24:25 -0300

    feat: class queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        ...
+        self.queue = []
 
     def __len__(self):
-        ...
+        return len(self.queue)
 
     def enqueue(self, value):
-        ...
+        self.queue.append(value)
 
     def dequeue(self):
-        ...
+        if self.is_empty():
+            return None
+        return self.queue.pop(0)
 
     def search(self, index):
-        ...
+        if index < 0 or index >= len(self.queue):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.queue[index]
+
+    def is_empty(self):
+        return len(self.queue) == 0
